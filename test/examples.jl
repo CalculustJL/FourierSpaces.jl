c162a274b7084fbcc48e66ea177f811f8ed8b4a8
Vedant Puri <vedantpuri@gmail.com>
2023-04-27 09:12:53 -0400

    update for scimlops 0.2

--- test/examples.jl
+++ test/examples.jl
@@ -1,6 +1,5 @@
 #
 @testset "1D tests" begin
-
     @time @safetestset "Advection" begin include("../examples/fourier1d/advect.jl") end
     @time @safetestset "Advection trans" begin include("../examples/fourier1d/advect_trans.jl") end
     @time @safetestset "Heat" begin include("../examples/fourier1d/heat.jl") end
@@ -12,7 +11,6 @@ end
     @time @safetestset "Advection" begin include("../examples/fourier2d/advect.jl") end
     @time @safetestset "Heat" begin include("../examples/fourier2d/heat.jl") end
     @time @safetestset "Heat forcing" begin include("../examples/fourier2d/heat_forcing.jl") end
-    @time @safetestset "Heat trans" begin include("../examples/fourier2d/heat_trans.jl")
-    end
+    @time @safetestset "Heat trans" begin include("../examples/fourier2d/heat_trans.jl") end
 end
 #
