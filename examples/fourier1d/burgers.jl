425bcedf9cdd81d7e660cf6bc3c2893229cba36a
Vedant Puri <vedantpuri@gmail.com>
2023-09-26 16:34:55 -0400

    clean up examples

--- examples/fourier1d/burgers.jl
+++ examples/fourier1d/burgers.jl
@@ -8,25 +8,33 @@ let
     nothing
 end
 
-using OrdinaryDiffEq, LinearAlgebra, Random
+using CUDA, OrdinaryDiffEq, LinearAlgebra, Random
 using Plots
 
+Random.seed!(0)
+CUDA.allowscalar(false)
+
 N = 1024
-ν = 1e-2
+Nmodes = 10
+ν = 1e-3
 p = nothing
 
-Random.seed!(0)
 function uIC(V::FourierSpace)
     x = points(V)[1]
-    X = truncationOp(V, (1/8,))
+    X = truncationOp(V, (Nmodes/N,))
 
-    u0 = X * rand(size(x)...)
-    u0 = @. cos(x+pi/2)
+    u0 = if x isa CUDA.CuArray
+        X * CUDA.rand(size(x)...)
+    else
+        X * rand(size(x)...)
+    end
+
+    u0
 end
 
 odecb = begin
     function affect!(int)
-        if int.iter % 100 == 0
+        if int.iter % 10 == 0
             println("[$(int.iter)] \t Time $(round(int.t; digits=8))s")
         end
     end
@@ -35,15 +43,16 @@ odecb = begin
 end
 
 function solve_burgers1D(N, ν, p;
-                         uIC=uIC,
-                         tspan=(0.0, 5.0),
-                         nsims=1,
-                         nsave=100,
-                         odealg=SSPRK43(),
-                        )
+    uIC=uIC,
+    tspan=(0.f0, 5.f0),
+    nsims=1,
+    nsave=100,
+    odealg=SSPRK43(),
+    device = cpu,
+)
 
     """ space discr """
-    V = FourierSpace(N)
+    V = FourierSpace(N) |> device
     discr = Collocation()
 
     (x,) = points(V)
@@ -59,27 +68,29 @@ function solve_burgers1D(N, ν, p;
     end
 
     function forcing!(f, u, p, t)
+#       f .= 1e-2*rand(length(f))
         lmul!(false, f)
     end
 
+    # model setup
     A = -diffusionOp(ν, V, discr)
     C = advectionOp((zero(u0),), V, discr; vel_update_funcs! = (burgers!,))
     F = forcingOp(zero(u0), V, discr; f_update_func=forcing!)
-    odefunc = cache_operator(A-C+F, u0) |> ODEFunction
+
+    odefunc = cache_operator(A-C+F, u0)
 
     """ time discr """
     tsave = range(tspan...; length=nsave)
-    prob = ODEProblem(odefunc, u0, tspan, p; reltol=1e-8, abstol=1e-8)
+    prob = ODEProblem(odefunc, u0, tspan, p; reltol=1f-6, abstol=1f-6)
     @time sol = solve(prob, odealg, saveat=tsave, callback=odecb)
 
     sol, V
 end
 
-sol, V = solve_burgers1D(N, ν, p)
+sol, V = solve_burgers1D(N, ν, p; device = cpu)
 V = cpu(V)
 pred = Array(sol)
 
-anim = animate(pred[:,1,:], V, sol.t,
-               legend=false, linewidth=2, color=:black, xlabel="x", ylabel="u(x,t)")
+anim = animate(pred[:,1,:], V, sol.t, legend=false, linewidth=2, color=:black, xlabel="x", ylabel="u(x,t)")
 gif(anim, joinpath(dirname(@__FILE__), "burgers.gif"), fps=20)
 #
--- examples/fourier1d/burgers_trans.jl
+++ examples/fourier1d/burgers_trans.jl
@@ -1,23 +1,20 @@
 #
 using FourierSpaces
-let
-    # add dependencies to env stack
-    pkgpath = dirname(dirname(pathof(FourierSpaces)))
-    tstpath = joinpath(pkgpath, "test")
-    !(tstpath in LOAD_PATH) && push!(LOAD_PATH, tstpath)
-    nothing
-end
 
-using OrdinaryDiffEq, LinearSolve, LinearAlgebra
+using OrdinaryDiffEq, LinearAlgebra, CUDA
 using Plots, Test
 
 N = 1024
+Nmodes = 10
 ν = 1e-3
 p = nothing
 
+device = gpu
+device = cpu
+
 odecb = begin
     function affect!(int)
-        if int.iter % 100 == 0
+        if int.iter % 10 == 0
             println("[$(int.iter)] \t Time $(round(int.t; digits=8))s")
         end
     end
@@ -26,7 +23,7 @@ odecb = begin
 end
 
 """ space discr """
-V  = FourierSpace(N)
+V  = FourierSpace(N) |> device
 Vh = transform(V)
 discr  = Collocation()
 
@@ -35,12 +32,19 @@ discr  = Collocation()
 F    = transformOp(V)
 
 """ initial condition """
-function uIC(V)
+function uIC(V::FourierSpace)
     x = points(V)[1]
-    X = truncationOp(V, (32/N,))
+    X = truncationOp(V, (Nmodes/N,))
 
-    u0 = X * rand(size(x)...)
+    u0 = if x isa CUDA.CuArray
+        X * CUDA.rand(size(x)...)
+    else
+        X * rand(size(x)...)
+    end
+
+    u0
 end
+
 u0 = uIC(V)
 û0 = F * u0
 
@@ -53,32 +57,18 @@ function forcing!(v, u, p, t)
 end
 
 Â = -diffusionOp(ν, Vh, discr)
-Ĉ = advectionOp((zero(û0),), Vh, discr; vel_update_funcs! =(burgers!,))
-F̂ = forcingOp(zero(û0), Vh, discr; f_update_func! = forcing!)
+Ĉ = advectionOp((zero(û0),), Vh, discr; vel_update_funcs! = (burgers!,))
+F̂ = forcingOp(zero(û0), Vh, discr; f_update_func=forcing!)
 
 Dt = cache_operator(Â-Ĉ+F̂, û0)
-
-Dt_implicit = cache_operator(Â, û0)
-Dt_explicit = cache_operator(-Ĉ+F̂, û0)
-
-dudt_implicit = function(du, u, p, t)
-    Dt_implicit(du, u, p, t)
-end
-
-dudt_explicit = function(du, u, p, t)
-    Dt_explicit(du, u, p, t)
-end
-
-Dt = SplitFunction(dudt_implicit, dudt_explicit; jac_prototype=Diagonal(û0))
 
 """ time discr """
 tspan = (0.0, 10.0)
 tsave = range(tspan...; length=100)
-odealg = Tsit5()
-# odealg = TRBDF2(autodiff=false, linsolve=KrylovJL_GMRES())
+odealg = SSPRK43()
 prob = ODEProblem(Dt, û0, tspan, p)
 
-@time sol = solve(prob, odealg, saveat=tsave, callback=odecb)
+@time sol = solve(prob, odealg, saveat=tsave, callback=odecb, reltol=1f-6, abstol=1f-6)
 
 """ analysis """
 pred = [F,] .\ sol.u
